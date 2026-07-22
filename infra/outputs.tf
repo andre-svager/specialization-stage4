444fdef3ede5d437dc49a2c7a02f4b232908770b
andre-svager <andreallet@gmail.com>
2026-07-22 13:20:11 -0300

    Add iam audience to gitactions

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -12,3 +12,13 @@ output "private_subnet_ids" {
   description = "Private subnet IDs"
   value       = module.networking.private_subnet_ids
 }
+
+output "workload_identity_provider" {
+  description = "Workload Identity Provider path for GitHub Actions"
+  value       = module.iam.workload_identity_provider
+}
+
+output "ci_service_account" {
+  description = "Email of the CI/CD service account for GitHub Actions"
+  value       = module.iam.ci_service_account
+}
